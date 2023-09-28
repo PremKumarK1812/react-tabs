7e44457bf3c6b91a3002844224867d2cffc65d5a
PremKumarK1812 <121390534+PremKumarK1812@users.noreply.github.com>
2023-09-28 21:35:02 +0530

    css work done

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,4 +1,5 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
+import { useForm } from 'react-hook-form';
 import { NavLink, useNavigate } from 'react-router-dom';
 import { signInWithEmailAndPassword, signInWithPopup } from 'firebase/auth';
 import { auth, googleProvider } from '../firebase-config';
@@ -7,20 +8,66 @@ const Login = () => {
   const navigate = useNavigate();
   const [email, setEmail] = useState();
   const [password, setPassword] = useState();
+  const [showCredentials, setShowCredentials] = useState(false);
 
-  const onLogin = async (e) => {
-    e.preventDefault();
-    await signInWithEmailAndPassword(auth, email, password)
-      .then((userCredential) => {
-        const user = userCredential.user;
-        setEmail('');
-        setPassword('');
-        navigate('/home');
-      })
-      .catch((err) => {
-        console.log('An error occurred', err.message);
-      });
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm();
+
+  const passwordMatch = useRef('');
+  passwordMatch.current = watch('password');
+
+  const validation = {
+    email: {
+      required: {
+        value: true,
+        message: 'Email is Mandatory',
+      },
+      pattern: {
+        value: /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+        message: 'Enter a valid Email address',
+      },
+    },
+    password: {
+      required: {
+        value: true,
+        message: 'Password is Mandatory',
+      },
+      minLength: {
+        value: 8,
+        message: 'Your password should contain atleast 8 characters',
+      },
+      maxLength: {
+        value: 16,
+        message: 'Password should have only 16 characters',
+      },
+    },
   };
+
+  const toggleCredentials = (e) => {
+    e.preventDefault();
+    setShowCredentials((showCredentials) => !showCredentials);
+  };
+
+  const onLogin = handleSubmit(async (e) => {
+    if (e.email && e.password) {
+      await signInWithEmailAndPassword(auth, e.email, e.password)
+        .then((userCredential) => {
+          const user = userCredential.user;
+          setEmail('');
+          setPassword('');
+          alert('Logged in Successfully!!!');
+          navigate('/home');
+        })
+        .catch((err) => {
+          alert('Log in Failed');
+          console.log('An error occurred', err.message);
+        });
+    }
+  });
 
   const googleSignUp = async (e) => {
     e.preventDefault();
@@ -31,9 +78,11 @@ const Login = () => {
         setEmail('');
         setPassword('');
         navigate('/home');
+        alert('Logged in Successfully!!!');
         console.log(user);
       })
       .catch((err) => {
+        alert('Log in Failed');
         console.log('An error occurred', err.message);
       });
   };
@@ -43,7 +92,7 @@ const Login = () => {
         <div>
           <div className='content'>
             <form className='form'>
-              <h1 className='title-1'>React Tabs</h1>
+              <h1 className='title-1'>Login</h1>
               <div className='email'>
                 <label htmlFor='email-address'>Email Address</label>
                 <input
@@ -51,9 +100,12 @@ const Login = () => {
                   id='email-address'
                   value={email}
                   onChange={(e) => setEmail(e.target.value)}
-                  required
                   placeholder='Email Address'
+                  {...register('email', validation.email)}
                 />
+                <p className='error-msg'>
+                  {errors.email && errors.email.message}
+                </p>
               </div>
               <div className='password'>
                 <label htmlFor='password'>Password</label>
@@ -62,11 +114,14 @@ const Login = () => {
                   id='password'
                   value={password}
                   onChange={(e) => setPassword(e.target.value)}
-                  required
                   placeholder='Password'
+                  {...register('password', validation.password)}
                 />
               </div>
-              <button type='submit' onClick={onLogin} className='login-button'>
+              <p className='error-msg'>
+                {errors.password && errors.password.message}
+              </p>
+              <button type='submit' className='login-button' onClick={onLogin}>
                 Login
               </button>
               <button
@@ -74,7 +129,7 @@ const Login = () => {
                 onClick={googleSignUp}
                 className='signin-button'
               >
-                Login with Google
+                Sign in with Google
               </button>
               <p className='account'>
                 Don't have an account?{' '}
@@ -82,6 +137,22 @@ const Login = () => {
                   Sign up
                 </NavLink>
               </p>
+              <button
+                className='show-credentials-button'
+                onClick={toggleCredentials}
+              >
+                User Credentials
+              </button>
+              {showCredentials && (
+                <div className='test-credentials'>
+                  <p>
+                    <strong>Email:</strong> abcd@test.com
+                  </p>
+                  <p>
+                    <strong>Password:</strong> Abcd!123
+                  </p>
+                </div>
+              )}
             </form>
           </div>
         </div>
--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -1,30 +1,108 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
+import { useForm } from 'react-hook-form';
 import { NavLink, useNavigate } from 'react-router-dom';
 import { createUserWithEmailAndPassword, signInWithPopup } from 'firebase/auth';
 import { auth, googleProvider } from '../firebase-config';
 
 const SignUp = () => {
   const navigate = useNavigate();
+  const [fullName, setFullName] = useState();
   const [email, setEmail] = useState();
   const [password, setPassword] = useState();
+  const [confirmPassword, setConfirmPassword] = useState();
 
-  const onSubmit = async (e) => {
-    e.preventDefault();
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm();
 
-    await createUserWithEmailAndPassword(auth, email, password)
-      .then((userCredential) => {
-        const user = userCredential.user;
-        setEmail('');
-        setPassword('');
-        navigate('/login');
-        console.log(user);
-      })
-      .catch((err) => {
-        console.log('An error occurred', err.message);
-      });
+  const passwordMatch = useRef('');
+  passwordMatch.current = watch('password');
 
-      
+  const onSubmit = async (data, e) => {
+    console.log(data);
+    if (data.fullName && data.email && data.password) {
+      e.preventDefault();
+
+      await createUserWithEmailAndPassword(auth, data.email, data.password)
+        .then((userCredential) => {
+          const user = userCredential.user;
+          console.log(user);
+          setEmail('');
+          setPassword('');
+          navigate('/login');
+          alert('Signed Up Successfully!!!');
+          console.log(user);
+        })
+        .catch((err) => {
+          alert('Sign up Failed');
+          console.log('An error occurred', err.message);
+        });
+    }
   };
+
+  const validation = {
+    fullName: {
+      required: {
+        value: true,
+        message: 'Enter your Full Name',
+      },
+      minLength: {
+        value: 5,
+        message: 'Your name should contain atleast 5 characters',
+      },
+      maxLength: {
+        value: 15,
+        message: 'Your name should have only 10 characters',
+      },
+    },
+    email: {
+      required: {
+        value: true,
+        message: 'Email is Mandatory',
+      },
+      pattern: {
+        value: /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+        message: 'Enter a valid Email address',
+      },
+    },
+    password: {
+      required: {
+        value: true,
+        message: 'Password is Mandatory',
+      },
+      minLength: {
+        value: 8,
+        message: 'Your password should contain atleast 8 characters',
+      },
+      maxLength: {
+        value: 16,
+        message: 'Password should have only 16 characters',
+      },
+    },
+    confirmPassword: {
+      required: {
+        value: true,
+        message: 'Confirm Password is Mandatory',
+      },
+      minLength: {
+        value: 8,
+        message: 'Your password should contain atleast 8 characters',
+      },
+      maxLength: {
+        value: 16,
+        message: 'Confirm Password should have only 16 characters',
+      },
+      validate: (value) => {
+        if (value !== passwordMatch.current) {
+          return "The passwords doesn't match";
+        }
+      },
+    },
+  };
+
   const googleSignUp = async (e) => {
     e.preventDefault();
 
@@ -33,20 +111,37 @@ const SignUp = () => {
         const user = userCredential.user;
         setEmail('');
         setPassword('');
+        alert('Logged in Successfully!!!');
         navigate('/home');
         console.log(user);
       })
       .catch((err) => {
+        alert('Log in Failed');
         console.log('An error occurred', err.message);
       });
   };
+
   return (
     <main>
       <section>
         <div>
           <div className='content'>
-            <form className='form'>
-              <h1 className='title-1'>React Tabs</h1>
+            <form className='form' onSubmit={handleSubmit(onSubmit)}>
+              <h1 className='title-1'>Sign Up</h1>
+              <div className='fullname'>
+                <label htmlFor='full-name'>Full Name</label>
+                <input
+                  type='text'
+                  id='full-name'
+                  value={fullName}
+                  onChange={(e) => setFullName(e.target.value)}
+                  placeholder='Full Name'
+                  {...register('fullName', validation.fullName)}
+                />
+                <p className='error-msg'>
+                  {errors.fullName && errors.fullName.message}
+                </p>
+              </div>
               <div className='email'>
                 <label htmlFor='email-address'>Email Address</label>
                 <input
@@ -55,24 +150,48 @@ const SignUp = () => {
                   value={email}
                   onChange={(e) => setEmail(e.target.value)}
                   placeholder='Email Address'
-                  required
+                  {...register('email', validation.email)}
                 />
+                <p className='error-msg'>
+                  {errors.email && errors.email.message}
+                </p>
               </div>
               <div className='password'>
-                <label htmlFor='create-password'>Create Password</label>
+                <label htmlFor='create-password'>Password</label>
                 <input
                   type='password'
-                  id='create-password'
+                  id='password'
                   value={password}
                   onChange={(e) => setPassword(e.target.value)}
                   placeholder='Password'
-                  required
+                  {...register('password', validation.password)}
                 />
+                <p className='error-msg'>
+                  {errors.password && errors.password.message}
+                </p>
               </div>
-              <button type='submit' onClick={onSubmit} className='signup-button'>
+              <div className='confirm-password'>
+                <label htmlFor='confirm-password'>Confirm Password</label>
+                <input
+                  type='password'
+                  id='confirm-password'
+                  value={confirmPassword}
+                  onChange={(e) => setConfirmPassword(e.target.value)}
+                  placeholder='Confirm Password'
+                  {...register('confirmPassword', validation.confirmPassword)}
+                />
+                <p className='error-msg'>
+                  {errors.confirmPassword && errors.confirmPassword.message}
+                </p>
+              </div>
+              <button type='submit' className='signup-button'>
                 Sign up
               </button>
-              <button type='submit' onClick={googleSignUp} className='signin-button'>
+              <button
+                type='submit'
+                onClick={googleSignUp}
+                className='signin-button'
+              >
                 Sign up with Google
               </button>
               <p className='account'>
